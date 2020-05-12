bee9b1ab28b3e7e109b67b762d7982230897ee56
Adam Osburn <wizardofosburn@gmail.com>
2020-05-12 19:32:29 -0400

    added state, onSubmit, button

--- src/Form.js
+++ src/Form.js
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+const {formState, setFormState} = useState({
+    name: '',
+    email: '',
+    password: '',
+    terms: '',,
+})
+
+const inputValue = setFormState({...formState, [e.target.name]: e.target.value})
 
 export default function Form(){
 
     return (
-        <form value='something'>
+        <form value='something' onSubmit={e.preventDefault()}>
             <label htmlFor='name'>
-                <input name='name' type='text' value={} onChange={(()=>something)} />
+                <input name='name' type='text' value={inputValue} onChange={(()=>something)} />
             </label>
             <label htmlFor='email'>
-                <input name='email' type='email' value={} onChange={(()=>something)} />
+                <input name='email' type='email' value={inputValue} onChange={(()=>something)} />
             </label>
             <label htmlFor='password'>
-                <input name='password' type='password' value={} onChange={(()=>something)} />
+                <input name='password' type='password' value={inputValue} onChange={(()=>something)} />
             </label>
             <label htmlFor='terms'>
                 Terms and Services
-                <input name='terms' type='checkbox' value={} onChange={(()=>something)} />
+                <input name='terms' type='checkbox' value={inputValue} onChange={(()=>something)} />
             </label>
+            <button>Turn it in!</button>
         </form>
     )
 }
