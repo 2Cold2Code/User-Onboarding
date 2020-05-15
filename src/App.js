e844fd17e7f2df0c27958bfd2c2564dc7f70f404
Adam Osburn <wizardofosburn@gmail.com>
2020-05-14 23:13:19 -0400

    set up state and useEffect note: useEffect questionable

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Form from './Components/Form';
+import Members from './Components/Members';
 import './App.css';
-import Form from './Form';
 
 function App() {
   //set up state for the member form:
-  const [user, serUser] = useState([])
+  const [user, setUser] = useState([]) // will need to set user state in object form within an array, since it expects an array
   
+  // set up state for the member:
+  const [member, setMember] = useState({...user});
+  console.log('user = ', user, '\nmember = ', member);
+
+  useEffect(
+    (e) => {
+      setMember({...user[0], [e.target.name]: e.target.value});
+    },
+    [user]
+  )
 
   return (
     <div className="App">
       <header className="App-header">
-        <Form />
+        <Form user={user} setUser={setUser} />
         <Members member={member} /> 
         {
           //sets the props to the member after member is made by form
