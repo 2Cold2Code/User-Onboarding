a666003be3aef6f297ecb7bdbac3d77031235ad8
Adam Osburn <wizardofosburn@gmail.com>
2020-05-14 22:57:54 -0400

    deleted all files and reverted code to last commit remaking Members.js and Form.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Form from './Form';
 
 function App() {
+  //set up state for the member form:
+  const [user, serUser] = useState([])
+  
+
   return (
     <div className="App">
       <header className="App-header">
         <Form />
+        <Members member={member} /> 
+        {
+          //sets the props to the member after member is made by form
+        }
       </header>
     </div>
   );
