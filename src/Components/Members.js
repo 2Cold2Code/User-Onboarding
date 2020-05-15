88741b75f78e425f247506617d1a576572a60e86
Adam Osburn <wizardofosburn@gmail.com>
2020-05-14 23:14:06 -0400

    set up props and block to render member elements

--- src/Components/Members.js
+++ src/Components/Members.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
-export default const Members = (props) => {
+const Members = (props) => {
     const {member} = props;
     
     return (
-        
-    )
+        <div>        {
+            member.map(member => (
+            <div>
+                <h3 className="member-name">{member.name}</h3>
+                <p className="member-email">{member.email}</p>
+                <p className="member-role">{member.role}</p>
+            </div>
+        )
+      )
+    }
+    </div>
+  )
 }
+export default Members
