03859b5de39a9865e9c9b1e8648c4744292f45e6
Adam Osburn <wizardofosburn@gmail.com>
2020-05-17 17:01:58 -0400

    created variable for state, added a route to ... Form and Members components created an array form member list testing purposes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React, { useState, useEffect } from 'react';
+import {Route, useHistory} from 'react-router-dom';
 import Form from './Components/Form';
 import Members from './Components/Members';
 import './App.css';
 
 function App() {
+
+  const baseStateObj = { // set the base properties for state to be reused
+    name: '',
+    email: '',
+    password: '',
+    terms: false
+  }
   //set up state for the member form:
-  const [user, setUser] = useState([]) // will need to set user state in object form within an array, since it expects an array
+  const [user, setUser] = useState(baseStateObj) // will need to set user state in object form within an array, since it expects an array
   
-  // set up state for the member:
-  const [member, setMember] = useState({...user});
-  console.log('user = ', user, '\nmember = ', member);
+  // set up state for the member display:
+  const [members, setMembers] = useState([baseStateObj]);
+
+  const memberList = []; // set up an array to hold members after validation.
+  console.log('user = ', user, '\nmember = ', members);
 
   useEffect(
     (e) => {
-      setMember({...user[0], [e.target.name]: e.target.value});
+      setMembers({...user, [user.name]: user.value});
+      console.log('!i \nuser: ', user, '\nmembers: ', members, '\n\ni!')
     },
     [user]
   )
@@ -21,12 +32,24 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Form user={user} setUser={setUser} />
-        <Members member={member} /> 
+        <Route exact path='/'>
+          <Form 
+          user={user} 
+          setUser={setUser} 
+          members={members} 
+          setMembers={setMembers} 
+          memberList={memberList}/>
+        </Route>
         {
           //sets the props to the member after member is made by form
         }
       </header>
+      <Route path='/members'>
+        <Members 
+        members={[members]} 
+        baseStateObj={baseStateObj}
+        memberList={memberList}/>
+      </Route>
     </div>
   );
 }
