2619159c14f7122269ed4f7cb1a2e0e23f6442a8
Adam Osburn <wizardofosburn@gmail.com>
2020-05-17 17:05:42 -0400

    added classname to div and used array for ... members upon submit, and... added keys for mapping

--- src/Components/Members.js
+++ src/Components/Members.js
@@ -1,16 +1,21 @@
 import React from 'react';
 
 const Members = (props) => {
-    const {member} = props;
+    console.log('props to Members before destructuring: ', props)
+    const {members, memberList} = props;
+    console.log(memberList)
+    memberList.push(...members)
+    console.log('props to Members: ', props)
+    console.log('"members" in Members: \n', members)
     
     return (
-        <div>        {
-            member.map(member => (
-            <div>
-                <h3 className="member-name">{member.name}</h3>
-                <p className="member-email">{member.email}</p>
-                <p className="member-role">{member.role}</p>
-            </div>
+        <div className='member-container App-header'>
+            {
+              memberList.map(member => (
+              <div key={`memberdiv${Date.now()}`}>
+                  <h3 className="member-name" key={`name${Date.now()}`}>{member.name}</h3>
+                  <p className="member-email" key={`email${Date.now()}`}>{member.email}</p>
+              </div>
         )
       )
     }
