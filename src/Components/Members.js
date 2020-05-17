212869bc001f96caaf0aff219a1da0ce57c36ddb
Adam Osburn <wizardofosburn@gmail.com>
2020-05-17 18:23:11 -0400

    added new member button with path to '/form'... placed it in a div... changed a div tag to a section tag

--- src/Components/Members.js
+++ src/Components/Members.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import {useHistory} from 'react-router-dom';
 
 const Members = (props) => {
+
+  const {push} = useHistory();
+  const newMember = () => {
+    push('/')
+  }
     console.log('props to Members before destructuring: ', props)
     const {members, memberList} = props;
     console.log(memberList)
@@ -9,7 +15,9 @@ const Members = (props) => {
     console.log('"members" in Members: \n', members)
     
     return (
-        <div className='member-container App-header'>
+      <div className='member-wrapper App-header'>
+        <button onSubmit={() => newMember()} >New Member</button>
+        <section className='member-container'>
             {
               memberList.map(member => (
               <div key={`memberdiv${Date.now()}`}>
@@ -19,6 +27,7 @@ const Members = (props) => {
         )
       )
     }
+    </section>
     </div>
   )
 }
